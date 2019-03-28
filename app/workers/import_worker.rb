009c6ddd94bfac29199e0019684125d3210b32a9
YoheiZuho <yuuman002@gmail.com>
2019-03-28 22:33:50 +0900

    フォローインポート関連を本家に追従

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -1,31 +1,14 @@
 # frozen_string_literal: true
 
-class Import::RelationshipWorker
+class ImportWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: 'pull', retry: 8, dead: false
+  sidekiq_options queue: 'pull', retry: false
 
-  def perform(account_id, target_account_uri, relationship)
-    from_account   = Account.find(account_id)
-    target_account = ResolveAccountService.new.call(target_account_uri)
-
-    return if target_account.nil?
-
-    case relationship
-    when 'follow'
-      FollowService.new.call(from_account, target_account)
-    when 'unfollow'
-      UnfollowService.new.call(from_account, target_account)
-    when 'block'
-      BlockService.new.call(from_account, target_account)
-    when 'unblock'
-      UnblockService.new.call(from_account, target_account)
-    when 'mute'
-      MuteService.new.call(from_account, target_account)
-    when 'unmute'
-      UnmuteService.new.call(from_account, target_account)
-    end
-  rescue ActiveRecord::RecordNotFound
-    true
+  def perform(import_id)
+    import = Import.find(import_id)
+    ImportService.new.call(import)
+  ensure
+    import&.destroy
   end
 end
